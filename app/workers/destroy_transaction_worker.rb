a877eccbaa0da7240c12d37f1439d633a81ea085
hagfayed <hagfayed@gmail.com>
2021-02-13 17:03:14 +0200

    fix bug of deleting conirmed transactions

--- app/workers/destroy_transaction_worker.rb
+++ app/workers/destroy_transaction_worker.rb
@@ -1,6 +1,8 @@
 class DestroyTransactionWorker
   include Sidekiq::Worker
   def perform(transaction_id)
-    TransactionHandlerService.new(transaction: Transaction.find(transaction_id)).handle_deleteion
+    transaction = Transaction.find(transaction_id)
+    return if transaction.confirmed_at.present?
+    TransactionHandlerService.new(transaction: transaction).handle_deleteion
   end
 end
